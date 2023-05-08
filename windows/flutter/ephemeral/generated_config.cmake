bcd72c007c09f9602eaa1fd93f49c6278b3da6f6
Daniel Lin <125861759+danielxiin1124gmail@users.noreply.github.com>
2023-05-09 07:59:55 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Danie\\myFlutterFile\\Github_AwesmeFlutter\\pokedex-master" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.4.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 4 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\Danie\\myFlutterFile\\Github_AwesmeFlutter\\pokedex-master"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Danie\\myFlutterFile\\Github_AwesmeFlutter\\pokedex-master\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Danie\\myFlutterFile\\Github_AwesmeFlutter\\pokedex-master"
+  "FLUTTER_TARGET=C:\\Users\\Danie\\myFlutterFile\\Github_AwesmeFlutter\\pokedex-master\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Danie\\myFlutterFile\\Github_AwesmeFlutter\\pokedex-master\\.dart_tool\\package_config.json"
+)
